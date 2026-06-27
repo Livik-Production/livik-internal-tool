69486f1d681d61ddbd7dee349f37e330fc731501
santhosh-liviktech <santhosh.n@liviktech.com>
2026-06-27 14:08:53 +0530

    Changes in companyDetails

--- app/components/Settings/CompanyDetailsTab.jsx
+++ app/components/Settings/CompanyDetailsTab.jsx
@@ -388,7 +388,7 @@ export default function CompanyDetailsTab() {
               <input
                 type="text"
                 name="cinNumber"
-                value={companyDetails.cinNumber}
+                value={companyDetails.cinNumber || ''}
                 onChange={handleCompanyDetailsChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-blue-500 outline-none"
               />
